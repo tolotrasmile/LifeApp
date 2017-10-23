3fb96619336a4b7ac47e39428907cc623bd13da3
Tolotra Raharison <tolotra.raharison@gmail.com>
2017-10-23 21:28:15 +0300

    Add EventBus and PreferencesHelper.

--- app/src/main/java/com/example/admin/lifeapp/MainActivity.kt
+++ app/src/main/java/com/example/admin/lifeapp/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.example.admin.lifeapp
 
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import org.androidannotations.annotations.Click
 import org.androidannotations.annotations.EActivity
-
+import org.greenrobot.eventbus.EventBus
+import com.example.admin.lifeapp.service.event.MessageEvent
+import com.example.admin.lifeapp.service.preference.PreferencesHelper
+import com.example.admin.lifeapp.service.preference.PreferencesHelper.set
+import com.example.admin.lifeapp.service.preference.PreferencesHelper.get
+import org.androidannotations.annotations.AfterViews
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import org.jetbrains.anko.toast
 
 @EActivity(R.layout.activity_main)
 open class MainActivity : AppCompatActivity() {
 
+    lateinit var prefs: SharedPreferences
+
+    @AfterViews
+    fun afterViews() {
+        prefs = PreferencesHelper.defaultPrefs(this)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this)
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        EventBus.getDefault().unregister(this)
+    }
+
     @Click(R.id.clickMe)
     open fun clickMe() {
+        val count = prefs["count", 0]
+        EventBus.getDefault().post(MessageEvent("Event $count"))
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onMessageEvent(event: MessageEvent) {
+        toast(event.message)
     }
 }
---
+++ app/src/main/java/com/example/admin/lifeapp/service/event/MessageEvent.kt
@@ -0,0 +1,6 @@
+package com.example.admin.lifeapp.service.event
+
+/**
+ * Created by admin on 23/10/2017.
+ */
+class MessageEvent(val message: String)
